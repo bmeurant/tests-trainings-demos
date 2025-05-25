ef9f8841fc0fdaa13839b1b73bb13ebaeae762a2
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-05-25 16:26:05 +0200

    chore(microservices-spring): init

---
+++ microservices-spring/src/main/java/io/bmeurant/microservices/spring/App.java
@@ -0,0 +1,13 @@
+package io.bmeurant.microservices.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
