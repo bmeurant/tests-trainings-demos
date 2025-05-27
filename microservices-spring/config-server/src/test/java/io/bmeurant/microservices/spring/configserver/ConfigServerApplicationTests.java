c50d94524f74b7369990ec6662fa1b877e9e5491
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-05-27 11:09:37 +0200

    chore(microservices-spring)(config-server): Init

---
+++ microservices-spring/config-server/src/test/java/io/bmeurant/microservices/spring/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bmeurant.microservices.spring.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
