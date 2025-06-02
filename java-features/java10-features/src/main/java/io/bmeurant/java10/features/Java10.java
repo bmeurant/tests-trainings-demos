369d8f044ed4cbf6ae2185d4b08fd1c38b896f5d
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-03 01:03:25 +0200

    feat(java10-features): init and add local variable type inference

---
+++ java-features/java10-features/src/main/java/io/bmeurant/java10/features/Java10.java
@@ -0,0 +1,8 @@
+package io.bmeurant.java10.features;
+
+public class Java10 {
+    public static void main(String[] args) {
+        System.out.println("\n--- Java 10: Local Variable Type Inference (var) ---");
+        LocalVariableTypeInference.main(args);
+    }
+}
