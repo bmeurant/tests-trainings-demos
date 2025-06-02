36eb736271ac1c07a3fc2e2c4234997328aab395
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-03 01:08:26 +0200

    feat(java10-features): add OptionalAPI improvements

--- java-features/java10-features/src/main/java/io/bmeurant/java10/features/Java10.java
+++ java-features/java10-features/src/main/java/io/bmeurant/java10/features/Java10.java
@@ -4,5 +4,8 @@ public class Java10 {
     public static void main(String[] args) {
         System.out.println("\n--- Java 10: Local Variable Type Inference (var) ---");
         LocalVariableTypeInference.main(args);
+
+        System.out.println("\n--- Java 10: Optional API Improvements ---");
+        OptionalImprovements.main(args);
     }
 }
