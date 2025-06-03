dcee896957919989e4f1b81efe3a6240a681c684
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-03 14:48:32 +0200

    feat(java12-features): add FilesMismatchMethod

---
+++ java-features/java12-features/src/main/java/io/bmeurant/java12/features/Java12.java
@@ -0,0 +1,8 @@
+package io.bmeurant.java12.features;
+
+public class Java12 {
+    public static void main(String[] args) {
+        System.out.println("\n--- Java 12: Files.mismatch() Method ---\n");
+        FilesMismatchMethod.main(args);
+    }
+}
