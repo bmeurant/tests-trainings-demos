486e20fafbeadacf76abf558d2c267b95d6da734
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-04 00:16:25 +0200

    feat(java16-features): add StrictFloatingPoint

--- java-features/java17-features/src/main/java/io/bmeurant/java17/features/Java17.java
+++ java-features/java17-features/src/main/java/io/bmeurant/java17/features/Java17.java
@@ -4,5 +4,8 @@ public class Java17 {
     public static void main(String[] args) {
         System.out.println("\n--- Java 17: Sealed Classes and Interfaces (Standard/GA) ---");
         SealedTypes.main(args);
+
+        System.out.println("\n--- Java 17: Restore Always-Strict Floating-Point Semantics (Standard/GA) ---");
+        StrictFloatingPoint.main(args);
     }
 }
