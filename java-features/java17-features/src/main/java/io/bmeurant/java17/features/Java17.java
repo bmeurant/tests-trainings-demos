e7c8592a7b709985225c8b730b3b333a280f19c7
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-04 00:14:25 +0200

    feat(java16-features): add SealedTypes

---
+++ java-features/java17-features/src/main/java/io/bmeurant/java17/features/Java17.java
@@ -0,0 +1,8 @@
+package io.bmeurant.java17.features;
+
+public class Java17 {
+    public static void main(String[] args) {
+        System.out.println("\n--- Java 17: Sealed Classes and Interfaces (Standard/GA) ---");
+        SealedTypes.main(args);
+    }
+}
