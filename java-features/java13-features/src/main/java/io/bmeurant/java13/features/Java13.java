c20b340444354f46c811192ea234f56ad1f6de80
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-03 15:18:39 +0200

    feat(java13-features): add SocketAPIImprovements

---
+++ java-features/java13-features/src/main/java/io/bmeurant/java13/features/Java13.java
@@ -0,0 +1,8 @@
+package io.bmeurant.java13.features;
+
+public class Java13 {
+    public static void main(String[] args) {
+        System.out.println("\n--- Java 13: Socket API Improvements (Under the Hood) ---\n");
+        SocketAPIImprovements.main(args);
+    }
+}
