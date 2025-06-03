05ef795220a56dedf7802017ab57bdf6c70cbcc7
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-03 22:48:14 +0200

    feat(java15-features): add TextBlocks

---
+++ java-features/java15-features/src/main/java/io/bmeurant/java15/features/Java15.java
@@ -0,0 +1,8 @@
+package io.bmeurant.java15.features;
+
+public class Java15 {
+    public static void main(String[] args) {
+        System.out.println("\n--- Java 15: Text Blocks  ---");
+        TextBlocks.main(args);
+    }
+}
