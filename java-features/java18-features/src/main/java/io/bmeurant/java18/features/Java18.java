5b00751e489034a87146e906fde504f2f53e88c2
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-04 08:29:58 +0200

    feat(java18-features): add UTF8ByDefault

---
+++ java-features/java18-features/src/main/java/io/bmeurant/java18/features/Java18.java
@@ -0,0 +1,8 @@
+package io.bmeurant.java18.features;
+
+public class Java18 {
+    public static void main(String[] args) {
+        System.out.println("\n--- Java 18: UTF-8 by Default (JEP 400) ---");
+        UTF8ByDefault.main(args);
+    }
+}
