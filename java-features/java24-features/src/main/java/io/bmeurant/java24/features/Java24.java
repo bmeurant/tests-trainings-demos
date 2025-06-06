acb5954a6ef3b4deee12dc73e120b7d1398b6eab
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-06 15:38:19 +0200

    feat(java24-features): add StreamGatherers

---
+++ java-features/java24-features/src/main/java/io/bmeurant/java24/features/Java24.java
@@ -0,0 +1,8 @@
+package io.bmeurant.java24.features;
+
+public class Java24 {
+    public static void main(String[] args) {
+        System.out.println("\n--- Java 24: Stream Gatherers (JEP 485) ---");
+        StreamGatherers.main(args);
+    }
+}
