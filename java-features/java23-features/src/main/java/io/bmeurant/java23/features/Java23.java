bfa82e8b8c901900fee21121427b1f7c81f6cd1a
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-06 15:12:24 +0200

    feat(java23-features): add MarkdownDocumentationComments

---
+++ java-features/java23-features/src/main/java/io/bmeurant/java23/features/Java23.java
@@ -0,0 +1,8 @@
+package io.bmeurant.java23.features;
+
+public class Java23 {
+    public static void main(String[] args) {
+        System.out.println("\n--- Java 23: Markdown Documentation Comments (JEP 467) ---");
+        MarkdownDocumentationComments.main(args);
+    }
+}
