43ccfd1d08bbeb2f70039f3437f7f87c6345f919
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-06 09:25:24 +0200

    feat(java22-features): add FFMApi

--- java-features/java22-features/src/main/java/io/bmeurant/java22/features/Java22.java
+++ java-features/java22-features/src/main/java/io/bmeurant/java22/features/Java22.java
@@ -4,5 +4,8 @@ public class Java22 {
     public static void main(String[] args) {
         System.out.println("\n--- Java 22: Unnamed Variables and Patterns (JEP 456) ---");
         UnnamedPatternsAndVariables.main(args);
+
+        System.out.println("\n--- Java 22: Foreign Function & Memory API (JEP 454) ---");
+        FFMApi.main(args);
     }
 }
