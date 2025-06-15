a68e9a623f61661d79d28e7eb29ea2d2a86ec84b
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-15 23:36:54 +0200

    chore(spring-boot30-features): init copying 2.7

---
+++ spring-boot-features/spring-boot30-features/src/main/java/io/bmeurant/spring/boot30/features/SpringBoot30Application.java
@@ -0,0 +1,13 @@
+package io.bmeurant.spring.boot30.features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot30Application.class, args);
+    }
+
+}
