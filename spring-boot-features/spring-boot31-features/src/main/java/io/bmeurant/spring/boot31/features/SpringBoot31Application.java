d85ffca1e8e1fe66ce017b50c232406e2fd8d1ed
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-17 11:49:50 +0200

    chore(spring-boot31-features): init

---
+++ spring-boot-features/spring-boot31-features/src/main/java/io/bmeurant/spring/boot31/features/SpringBoot31Application.java
@@ -0,0 +1,13 @@
+package io.bmeurant.spring.boot31.features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot31Application.class, args);
+    }
+
+}
---
+++ spring-boot-features/spring-boot31-features/src/main/java/io/bmeurant/spring/boot31/features/service/ProductService.java
@@ -0,0 +1,38 @@
+package io.bmeurant.spring.boot31.features.service;
+
+import io.bmeurant.spring.boot31.features.model.Product;
+import io.bmeurant.spring.boot31.features.repository.ProductRepository;
+import io.micrometer.observation.annotation.Observed;
+import io.micrometer.tracing.annotation.NewSpan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Observed(name = "productService")
+public class ProductService {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductService.class);
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Observed(name = "product.find-all", contextualName = "finding-all-products",
+            lowCardinalityKeyValues = {"component", "data-access"})
+    public List<Product> findAllProducts() {
+        log.info("ProductService: Fetching all products.");
+        return productRepository.findAll();
+    }
+
+    @Observed(name = "product.add-one", contextualName = "adding-single-product",
+            lowCardinalityKeyValues = {"operation", "write"})
+    public Product addProduct(Product product) {
+        log.info("ProductService: Adding product: {}", product.getName());
+        return productRepository.save(product);
+    }
+}
