126a28cda1b06ac0285f996b81b4ec71c1e2de40
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-27 17:25:59 +0200

    chore(ddd-bdd-tdd): init

---
+++ ddd-bdd-tdd/src/main/java/io/bmeurant/book/order/manager/BookOrderManagerApplication.java
@@ -0,0 +1,13 @@
+package io.bmeurant.book.order.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOrderManagerApplication.class, args);
+	}
+
+}
---
+++ ddd-bdd-tdd/src/test/java/io/bmeurant/book/order/manager/BookOrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bmeurant.book.order.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
