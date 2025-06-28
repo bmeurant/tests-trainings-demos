55e0256425b71ddc2e9445fb279c0785ae6eeed9
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-29 01:38:22 +0200

    booktest

--- ddd-bdd-tdd/src/test/java/io/bmeurant/bookordermanager/unit/catalog/book/domain/model/BookTest.java
+++ ddd-bdd-tdd/src/test/java/io/bmeurant/bookordermanager/unit/catalog/book/domain/model/BookTest.java
@@ -1,13 +1,13 @@
 package io.bmeurant.bookordermanager.unit.catalog.book.domain.model;
 
-import io.bmeurant.bookordermanager.catalog.Book;
+import io.bmeurant.bookordermanager.catalog.domain.model.Book;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BookConsistencyTest {
+public class BookTest {
 
     @Test
     void shouldCreateBookWithValidParameters() {
@@ -87,5 +87,47 @@ public class BookConsistencyTest {
             new Book("123-456", "Title", "Author", new BigDecimal("-0.01"));
         });
         assertTrue(exception.getMessage().contains("Price cannot be negative"));
+    }
+
+    @Test
+    void shouldBeEqualWhenIsbnIsSame() {
+        BigDecimal price1 = new BigDecimal("25.00");
+        BigDecimal price2 = new BigDecimal("30.00");
+
+        Book book1 = new Book("978-0321765723", "Title A", "Author A", price1);
+        Book book2 = new Book("978-0321765723", "Title B", "Author B", price2);
+
+        assertEquals(book1, book2);
+        assertEquals(book1.hashCode(), book2.hashCode());
+    }
+
+    @Test
+    void shouldNotBeEqualWhenIsbnIsDifferent() {
+        BigDecimal price = new BigDecimal("25.00");
+
+        Book book1 = new Book("978-0321765723", "Title A", "Author A", price);
+        Book book2 = new Book("978-0132350884", "Title A", "Author A", price);
+
+        assertNotEquals(book1, book2);
+        assertNotEquals(book1.hashCode(), book2.hashCode());
+    }
+
+    @Test
+    void shouldBeEqualWithSelf() {
+        Book book = new Book("978-0321765723", "Title", "Author", new BigDecimal("25.00"));
+        assertEquals(book, book);
+        assertEquals(book.hashCode(), book.hashCode());
+    }
+
+    @Test
+    void shouldNotBeEqualToNull() {
+        Book book = new Book("978-0321765723", "Title", "Author", new BigDecimal("25.00"));
+        assertNotEquals(null, book);
+    }
+
+    @Test
+    void shouldNotBeEqualToDifferentClass() {
+        Book book = new Book("978-0321765723", "Title", "Author", new BigDecimal("25.00"));
+        assertNotEquals(book, new Object());
     }
 }
