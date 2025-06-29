9f37c6e5442f493082604eaa30cfd84829663ed7
Baptiste Meurant <baptiste.meurant@gmail.com>
2025-06-30 01:28:25 +0200

    test(ddd-bdd-tdd): add test for OrderEventHandler (confirmation)

---
+++ ddd-bdd-tdd/src/test/java/io/bmeurant/bookordermanager/unit/order/domain/handler/OrderEventHandlerTest.java
@@ -0,0 +1,48 @@
+package io.bmeurant.bookordermanager.unit.order.domain.handler;
+
+import io.bmeurant.bookordermanager.order.domain.event.OrderCreatedEvent;
+import io.bmeurant.bookordermanager.order.domain.handler.OrderEventHandler;
+import io.bmeurant.bookordermanager.order.domain.model.Order;
+import io.bmeurant.bookordermanager.order.domain.repository.OrderRepository;
+import io.bmeurant.bookordermanager.order.domain.model.OrderLine;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class OrderEventHandlerTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Mock
+    private PlatformTransactionManager transactionManager;
+
+    @InjectMocks
+    private OrderEventHandler orderEventHandler;
+
+    @Test
+    void handleOrderCreatedEvent_shouldConfirmOrder() {
+        // Given
+        OrderLine orderLine = new OrderLine("978-0321765723", 1, BigDecimal.valueOf(25.00));
+        Order order = new Order("customer", List.of(orderLine));
+        OrderCreatedEvent event = new OrderCreatedEvent(order);
+        when(orderRepository.findById(order.getOrderId())).thenReturn(Optional.of(order));
+
+        // When
+        orderEventHandler.handleOrderCreatedEvent(event);
+
+        // Then
+        verify(orderRepository).save(argThat(savedOrder -> savedOrder.getStatus() == Order.OrderStatus.CONFIRMED));
+    }
+}
